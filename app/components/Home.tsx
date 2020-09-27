626e2186aeb3f8068c25f1b2575a978df9579cb8
Tech Assistants Arch <techassistantselm@gmail.com>
2020-09-27 10:10:07 +0100

    First Commit

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,97 @@
 import React from 'react';
+import mustache from 'mustache';
+import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import fs from 'fs';
+import path from 'path';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
+
+const { app, dialog } = require('electron').remote;
+
+function writeIt(html: string, dest) {
+  fs.writeFile(dest, html, function (err: any, data: any) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+  });
+}
+
+function saveHtml(html: string) {
+  const newHtml = JSON.stringify(html);
+  // app.getPath("desktop")       // User's Desktop folder
+  // app.getPath("documents")     // User's "My Documents" folder
+  // app.getPath("downloads")     // User's Downloads folder
+  // console.log(app);
+  console.log(app.getPath('downloads'));
+
+  const toLocalPath = path.resolve(
+    app.getPath('downloads'),
+    path.basename('Template.html')
+  );
+
+  console.log(dialog);
+
+  const userChosenPath = dialog
+    .showSaveDialog({
+      defaultPath: toLocalPath,
+      filters: [
+        {
+          name: 'Email Template (HTML)',
+          extensions: ['html'],
+        },
+      ],
+    })
+    .then((data) => {
+      if (data.filePath) {
+        // console.log(data.filePath);
+        writeIt(html, data.filePath);
+        return data.filePath;
+      }
+      return new Error("Profile doesn't exist");
+    });
+}
+
+function genHtml(html: string): void {
+  const names = {
+    Test: 'Hello World!',
+  };
+  const rendered = mustache.render(html, names);
+  saveHtml(rendered);
+  // console.log(`Rendered HTML ${rendered}`);
+}
+
+function getHtml(): void {
+  let data = '';
+
+  const readStream = fs.createReadStream(
+    `${__dirname}/templates/template.html`,
+    'utf8'
+  );
+  readStream
+    .on('data', function (chunk) {
+      data += chunk;
+    })
+    .on('end', function () {
+      genHtml(data);
+    });
+}
 
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+      <h2>Emailer</h2>
+      <br />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          getHtml();
+        }}
+      >
+        Run This
+      </Button>
     </div>
   );
 }
--- app/main.dev.ts
+++ app/main.dev.ts
@@ -16,6 +16,8 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 
+// require('@electron/remote/main').initialize()
+
 export default class AppUpdater {
   constructor() {
     log.transports.file.level = 'info';
